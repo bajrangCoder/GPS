fabdf366955cc32b151bcb5829c49eec7953a260
Raunak Raj <71929976+nkray-raunak123@users.noreply.github.com>
2022-03-15 12:51:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from kivy.utils import platform
 
 if platform == "android":
     from android.permissions import request_permissions, Permission
-    request_permissions([Permission.ACCESS_BACKGROUND_LOCATION])
+    request_permissions([Permission.ACCESS_FINE_LOCATION])
 
 
 kv = '''
